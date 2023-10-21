dffea67646cc776f420f58e593bd6aa714d1b8cd
CzAsQo <145834447+CzAsQo@users.noreply.github.com>
2023-10-21 11:41:57 +0200

    Portale Dzialaja

--- Assets/Scripts/PortalTeleport.cs
+++ Assets/Scripts/PortalTeleport.cs
@@ -10,4 +10,44 @@ public class PortalTeleport : MonoBehaviour
     
     private bool playerIsOverlapping = false;
 
+    private void OnTriggerEnter (Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerIsOverlapping = true;
+        }
+    }
+
+    void OntriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+        }
+    }
+
+    void Teleportation()
+    {
+        if (playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            if(dotProduct < 0f)
+            {
+                float rotationDiff = Quaternion.Angle(transform.rotation, receiver.rotation);
+                rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                player.position = receiver.position + positionOffset;
+                playerIsOverlapping = false;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Teleportation();
+    }
+
 }
